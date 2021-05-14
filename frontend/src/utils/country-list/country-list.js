4ccebf5a5870895312bba6a2189d2e444dc3d4ec
Suicidal <karim.mohamed420@gmail.com>
2021-05-14 22:37:53 +0200

    Update country-list.js

--- frontend/src/utils/country-list/country-list.js
+++ frontend/src/utils/country-list/country-list.js
@@ -1,78 +1,78 @@
 class CountryList {
-  constructor() {
-    this.data = require('./data.json')
-    this.labelMap = {}
-    this.valueMap = {}
+	constructor() {
+		this.data = require('./data.json');
+		this.labelMap = {};
+		this.valueMap = {};
 
-    this.data.forEach(country => {
-      this.labelMap[country.label.toLowerCase()] = country.value
-      this.valueMap[country.value.toLowerCase()] = country.label
-    })
-  }
+		this.data.forEach((country) => {
+			this.labelMap[country.label.toLowerCase()] = country.value;
+			this.valueMap[country.value.toLowerCase()] = country.label;
+		});
+	}
 
-  getValue(label) {
-    return this.labelMap[label.toLowerCase()]
-  }
+	getValue(label) {
+		return this.labelMap[label.toLowerCase()];
+	}
 
-  getLabel(value) {
-    return this.valueMap[value.toLowerCase()]
-  }
+	getLabel(value) {
+		return this.valueMap[value.toLowerCase()];
+	}
 
-  getLabels() {
-    return this.data.map(country => country.label)
-  }
+	getLabels() {
+		return this.data.map((country) => country.label);
+	}
 
-  getValues() {
-    return this.data.map(country => country.value)
-  }
+	getValues() {
+		return this.data.map((country) => country.value);
+	}
 
-  getLabelList() {
-    return this.labelMap
-  }
+	getLabelList() {
+		return this.labelMap;
+	}
 
-  getValueList() {
-    return this.valueMap
-  }
+	getValueList() {
+		return this.valueMap;
+	}
 
-  getData() {
-    return this.data
-  }
+	getData() {
+		return this.data;
+	}
 
-  setLabel(value, label) {
-    this.data.forEach(country => {
-      if (country.value === value) {
-        country.label = label
-        this.valueMap[country.value.toLowerCase()] = country.label
-      }
-    })
+	setLabel(value, label) {
+		this.data.forEach((country) => {
+			if (country.value === value) {
+				country.label = label;
+				this.valueMap[country.value.toLowerCase()] = country.label;
+			}
+		});
 
-    return this
-  }
+		return this;
+	}
 
-  setEmpty(label) {
-    this.data.unshift({
-      value: '',
-      label: label,
-    })
-    this.valueMap[''] = label
-    this.labelMap[label] = ''
+	setEmpty(label) {
+		this.data.unshift({
+			value: '',
+			label: label,
+		});
+		this.valueMap[''] = label;
+		this.labelMap[label] = '';
 
-    return this
-  }
+		return this;
+	}
 
-  native() {
-    this.nativeData = require('./data-native.json')
-    this.nativeData.forEach(country => {
-      this.labelMap[country.label.toLowerCase()] = country.value
-      this.valueMap[country.value.toLowerCase()] = country.label
-    })
+	native() {
+		this.nativeData = require('./data-native.json');
+		this.nativeData.forEach((country) => {
+			this.labelMap[country.label.toLowerCase()] = country.value;
+			this.valueMap[country.value.toLowerCase()] = country.label;
+		});
 
-    return this
-  }
+		return this;
+	}
 }
 
 const countryList = () => {
-  if (!(this instanceof CountryList)) return new CountryList()
-}
+	if (!(this instanceof CountryList)) return new CountryList();
+};
 
-module.exports = countryList
+export default countryList;
